d478c8e689e538a2136aa5f626184bc15c17243d
xAlessandroo <alessandro.iozzino1@gmail.com>
2024-04-30 17:08:11 +0200

    vite config deploy try

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/xAlessandroo/',
   plugins: [react()],
-})
+  build: {
+    assetsInlineLimit: 0,
+  },
+  server: {
+    hmr: {
+      port: 443,
+    },
+  },
+});
